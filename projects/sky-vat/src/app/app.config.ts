22c2ee2ed7786800f023733706a8640c3e0c34a4
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-02-10 18:42:26 +0600

    configaration bootstrap , toster ngzorro

--- projects/sky-vat/src/app/app.config.ts
+++ projects/sky-vat/src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes), provideNzI18n(en_US),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideHttpClient(
+      withJsonpSupport()
+    ),
+    provideToastr(), // Toastr providers
   ]
 };
--- projects/sky-vat/src/app/app.ts
+++ projects/sky-vat/src/app/app.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,NzCardModule,NzButtonModule,NzLayoutModule ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
