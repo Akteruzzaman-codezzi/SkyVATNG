3071df17efff2cc176ceb4f1e5bae648e03f5643
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-02-10 16:54:40 +0600

    Environment setup

---
+++ projects/sky-vat/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '',
+  apiTestBaseUrl: '',
+  apiTimeout: 30000,
+};
---
+++ projects/sky-vat/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://localhost:44393/api',
+  apiTestBaseUrl: 'https://localhost:44393/api',
+  apiTimeout: 30000
+};
