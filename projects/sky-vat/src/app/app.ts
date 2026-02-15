3c887280dd48895084a0e442d0b1a1c5383ed31d
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-02-15 16:46:04 +0600

    Issue update on routing

--- projects/sky-vat/src/app/app.ts
+++ projects/sky-vat/src/app/app.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,RouterModule,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
